3b00f91ee2fdf10751c9bdc167542221481afcba
codersanjay-inc <coder.sanjay19@gmail.com>
2022-11-19 20:46:20 +0530

    cupertino_icon

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:flutter_application_1/widgets/drawer.dart';
@@ -10,18 +11,27 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: ListView(
-      children: [
-        DrawerHeader(
-          padding: EdgeInsets.zero,
-          child: UserAccountsDrawerHeader(
-              accountName: const Text("Sanjay Maddheshiya"),
-              accountEmail: const Text("coder.sanjay19@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(imageUrl),
-              )),
-        )
-      ],
+        child: Container(
+      color: Colors.cyan,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                decoration: const BoxDecoration(color: Colors.cyan),
+                accountName: const Text("Sanjay Maddheshiya"),
+                accountEmail: const Text("coder.sanjay19@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                )),
+          ),
+          const ListTile(
+            leading: Icon(CupertinoIcons.home),
+          )
+        ],
+      ),
     ));
   }
 }
